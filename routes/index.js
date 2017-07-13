97e703f924f0bc2565791e2f058ee4a7e92727b4
mbr84 <matthew.bramfeld@gmail.com>
2017-07-13 14:01:27 -0700

    removed login

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+    res.render('index');
 });
 
 router.get('/register', function(req, res) {
